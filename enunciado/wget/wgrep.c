fb8389386aa91db68d27e06bcdfa460f3d4d07d0
praridz <rios.371@hotmail.com>
2020-09-20 21:21:22 -0500

    wgrep ready

--- enunciado/wget/wgrep.c
+++ enunciado/wget/wgrep.c
@@ -4,32 +4,49 @@
 
 int main(int argc, char *argv[])
 {
-    
+
     FILE *fp;
     char buff[1024];
-    char* word = argv[1]; //cogemos la palabra
-    if(argc < 3){
-          printf("wgrep: searchterm [file ...]");
-          exit(1);
-    }
-    
-    for (size_t i = 2; i < argc; i++)
+    char *word = argv[1]; //cogemos la palabra
+    if (argc < 2)
     {
-        fp = fopen(argv[i], "r");
-        if (fp == NULL)
-        {
-            printf("wgrep: cannot open file");
-            exit(1);
-        }
-        
-        while (fgets(buff, 1024, fp))
-        {   
-            if(strstr(buff,word) != NULL){
-                printf("%s", buff);
-            }
-            
-        }
-        fclose(fp);
+        printf("wgrep: searchterm [file ...]\n");
+        exit(1);
     }
+    if (argc < 3)
+    {
+            fp = stdin;
+            fscanf(fp, "%s", buff);
+            while (fgets(buff, 1024, fp))
+            {
+                if (strstr(buff, word) != NULL)
+                {
+                    printf("%s", buff);
+                }
+            }
+    }
+
+    if (strstr(" ", word) == NULL)
+    {
+        for (size_t i = 2; i < argc; i++)
+        {
+            fp = fopen(argv[i], "r");
+            if (fp == NULL)
+            {
+                printf("wgrep: cannot open file\n");
+                exit(1);
+            }
+
+            while (fgets(buff, 1024, fp))
+            {
+                if (strstr(buff, word) != NULL)
+                {
+                    printf("%s", buff);
+                }
+            }
+            fclose(fp);
+        }
+    }
+
     return 0;
 }
